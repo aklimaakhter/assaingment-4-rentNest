9fbf3d34ef0917f72bd53e3f075b5945ca7eddc7
aklimaakhter <aklima00191@gmail.com>
2026-08-01 13:57:18 +0600

    stripe setup

--- src/utils/cashAsync.ts
+++ src/utils/cashAsync.ts
@@ -1,20 +1,12 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
-import httpStatus from "http-status"
+
 
 export const catchAsync = (fn: RequestHandler) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         try {
             await fn(req, res, next)
         } catch (error) {
-
             next()
-            console.log(error)
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-                success: false,
-                statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-                message: "Failed to register user",
-                error: (error as Error).message
-            })
         }
     }
 }
